4ded900f869b509b7f5f4d0a53102d9f7b0085a0
JstVex <heinhtetlulu14@gmail.com>
2024-01-12 01:35:40 -0800

    Add Capabilities section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 import Main from "@/components/Layouts";
 import AboutMe from "@/components/About/AboutMe";
 import Education from "@/components/About/Education";
+import Capabilities from "@/components/About/Capabilities";
 
 const About = () => {
     return (
         <Main>
             <AboutMe />
             <Education />
+            <Capabilities />
         </Main>
     );
 }
---
+++ components/About/Capabilities.tsx
@@ -0,0 +1,97 @@
+import { ChevronRight, Minus } from "lucide-react";
+
+const Capabilities = () => {
+    return (
+        <section className="mt-16 my-12 pt-5 border-t border-zinc-900">
+            <h2 className="text-2xl font-semibold md:mb-3">
+                Capabilities
+            </h2>
+            <div>
+                <div className="pt-5 md:pb-10 md:flex md:justify-between md:items-start">
+                    <div className="text-lg my-3 md:my-0">
+                        <h3>
+                            Frontend
+                        </h3>
+                    </div>
+                    <ul className="flex flex-col space-y-3 md:w-divider">
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                HTML, CSS, JavaScript
+                            </span>
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                React, Next.js, Tailwind CSS
+                            </span>
+
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                Typescript, Figma, Framer Motion
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-5 md:pb-10 md:flex md:justify-between md:items-start">
+                    <div className="text-lg my-3 md:my-0">
+                        <h3>
+                            Backend
+                        </h3>
+                    </div>
+                    <ul className="flex flex-col space-y-3 md:w-divider">
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                Node.js, Express.js, REST API
+                            </span>
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                MongoDB, MySQL, Prisma, Planetscale
+                            </span>
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                JWT, NextAuth.js, Clerk
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-5 md:pb-10 md:flex md:justify-between md:items-start">
+                    <div className="text-lg my-3 md:my-0">
+                        <h3>
+                            Additional
+                        </h3>
+                    </div>
+                    <ul className="flex flex-col space-y-3 md:w-divider">
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                C++, Python, Mathematics
+                            </span>
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                IT Support, Command Line, Git
+                            </span>
+                        </li>
+                        <li className="text-zinc-200 list-none flex items-center gap-x-2">
+                            <Minus size={20} />
+                            <span>
+                                MS Office, Google Workspace
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Capabilities;
