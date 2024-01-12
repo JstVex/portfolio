f28364418eea03f709bf4f0111ba821b3920a6d6
JstVex <heinhtetlulu14@gmail.com>
2024-01-11 22:31:51 -0800

    Create About page with about me section

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Main from "@/components/Layouts";
+import AboutMe from "@/components/About/AboutMe";
+
+const About = () => {
+    return (
+        <Main>
+            <AboutMe />
+        </Main>
+    );
+}
+
+export default About;
---
+++ components/About/AboutMe.tsx
@@ -0,0 +1,19 @@
+const AboutMe = () => {
+    return (
+        <section>
+            <h1 className="text-3xl font-semibold">
+                About Me
+            </h1>
+            <div className="mt-6 text-base font-light tracking-wide leading-relaxed text-zinc-100 flex flex-col gap-3">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel deleniti fugiat minus cupiditate inventore reprehenderit impedit officiis! Vitae at unde debitis. Optio quis iure magnam eaque dolorem aperiam fugiat velit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit perspiciatis dicta ab, cum mollitia ea tempora.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut perspiciatis quae sapiente, ipsa eaque earum, cumque nam quia possimus sit autem placeat laudantium sed itaque! Porro quae voluptatum quod dicta!
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default AboutMe;
