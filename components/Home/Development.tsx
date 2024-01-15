390edcdd2daefdbeb2bba616573b1116ac808b5a
JstVex <heinhtetlulu14@gmail.com>
2024-01-15 15:41:17 -0800

    Add slug for link

--- components/Home/Development.tsx
+++ components/Home/Development.tsx
@@ -1,5 +1,6 @@
 import { ArrowRight } from "lucide-react";
 import Section from "../Layouts/Section";
+import Link from "next/link";
 
 interface developmentProps {
     inDevelopment: {
@@ -8,6 +9,7 @@ interface developmentProps {
         image: string;
         description: string;
         variety: "featured" | "in-development" | "frontend";
+        slugAsParams: string;
         link?: string;
         techStack?: string[];
     }[];
@@ -18,12 +20,12 @@ const Development: React.FC<developmentProps> = ({ inDevelopment }) => {
         <Section title="In Development">
             {inDevelopment.map((project) => (
                 <li key={project.title} className="flex flex-col space-y-1">
-                    <a href={project.link} className="flex items-center space-x-1">
-                        <h3 className="text-lg underline underline-offset-4 transition-colors decoration-zinc-700 hover:decoration-zinc-500">
+                    <Link href={`/projects/${project.slugAsParams}`} className="flex items-center space-x-1">
+                        <h3 className="underline underline-offset-4 transition-colors decoration-zinc-700 hover:decoration-zinc-500">
                             {project.title}
                         </h3>
                         <ArrowRight size={16} className="text-zinc-400" />
-                    </a>
+                    </Link>
                     <p className="text-zinc-400">
                         {project.description}
                     </p>
