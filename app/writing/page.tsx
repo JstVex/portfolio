0573369f4b6b53b740a4bdc20fb8c2d5f64b6460
JstVex <heinhtetlulu14@gmail.com>
2024-01-15 17:45:53 -0800

    Create writing page

---
+++ app/writing/page.tsx
@@ -0,0 +1,20 @@
+import Main from "@/components/Layouts";
+
+const Writing = () => {
+    return (
+        <Main>
+            <section>
+                <h1 className="text-3xl font-semibold">
+                    Writing
+                </h1>
+                <div className="mt-6 text-base font-light tracking-wide leading-relaxed text-zinc-100 flex flex-col gap-3">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos veniam tenetur quod accusamus, et voluptatem amet ullam, voluptatibus vero praesentium laborum dolorem, dolores beatae labore facere nisi aliquam consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </p>
+                </div>
+            </section>
+        </Main>
+    );
+}
+
+export default Writing;
