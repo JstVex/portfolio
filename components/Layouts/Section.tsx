b6fed24fe7413793d0deb10f5919cfc6dd49b605
JstVex <heinhtetlulu14@gmail.com>
2024-01-15 16:53:11 -0800

    Implement className prop

--- components/Layouts/Section.tsx
+++ components/Layouts/Section.tsx
@@ -1,15 +1,18 @@
+import { cn } from "@/lib/utils";
+
 interface SectionProps {
     title: string;
+    className?: string;
     children: React.ReactNode
 }
 
-const Section: React.FC<SectionProps> = ({ title, children }) => {
+const Section: React.FC<SectionProps> = ({ title, className, children }) => {
     return (
         <section className="pt-5 pb-16 border-t border-zinc-900 md:flex md:justify-between md:items-start">
             <h2 className="text-xl font-semibold my-3 md:my-0">
                 {title}
             </h2>
-            <ul className="flex flex-col gap-y-[0.85rem] md:w-divider">
+            <ul className={cn("flex flex-col gap-y-[0.85rem] md:w-divider", className)}>
                 {children}
             </ul>
         </section>
