fb89b5073e13d3a19a07bde5f68abc3570df3445
JstVex <heinhtetlulu14@gmail.com>
2024-01-17 18:49:43 -0800

    Add real data

--- components/Home/Intro.tsx
+++ components/Home/Intro.tsx
@@ -13,7 +13,7 @@ const Intro = () => {
                 </p>
             </div>
             <div className="mt-6 text-base font-light tracking-wide leading-relaxed text-zinc-100">
-                Hi, my name is Lu Lu. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores et autem id, commodi, velit veritatis illum voluptate non corrupti dolorem saepe deleniti laboriosam officiis accusantium dolor. Veniam totam molestiae quaerat? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis ullam minus eligendi!
+                Hi, my name is Lu Lu. I design and develop websites with the goal of solving problems, making impact and crafting compelling digital experiences. I am currently a freshman at Pasadena City College, learning new skills and honing old ones. I enjoy solving real-world problems and turning them into side projects. I am currently looking for internship opportunities for Summer 2024.
             </div>
             <div className="flex items-center mt-3">
                 <Link href='/about' className="flex items-center border-b border-zinc-400 text-zinc-100 cursor-pointer transition-colors hover:text-zinc-400 hover:border-zinc-500">
