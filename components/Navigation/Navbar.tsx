cbb50c4cd21242df385edae06a5d7add9d97b245
JstVex <heinhtetlulu14@gmail.com>
2024-01-17 17:41:54 -0800

    Update navbar

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Snowflake, Sun } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -14,20 +15,27 @@ const Navbar = () => {
     const pathname = usePathname();
 
     return (
-        <nav className="flex justify-center">
-            <div className="flex items-center w-full max-w-[60rem] py-4 px-3">
-                <div className="grow text-sm">
-                    Lu Lu
+        <nav className="flex justify-center py-3 px-4 sm:px-6">
+            <div className="w-full max-w-main grow">
+                <div className="flex items-center md:justify-between md:items-start">
+                    <div className="hover:animate-spin-slow">
+                        <Snowflake />
+                    </div>
+                    <ul className="ml-auto flex items-center text-sm md:w-divider">
+                        <div className="flex-auto flex items-center space-x-5">
+                            {pages.map((page) => (
+                                <li key={page.href} className="list-none">
+                                    <Link href={page.href} className={`nav-link ${pathname === page.href ? "" : "text-zinc-400"}`}>
+                                        {page.label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </div>
+                        <div className="hidden md:block hover:animate-spin-slow">
+                            <Sun />
+                        </div>
+                    </ul>
                 </div>
-                <ul className="flex items-center space-x-5 text-sm">
-                    {pages.map((page) => (
-                        <li key={page.href} className="list-none">
-                            <Link href={page.href} className={`nav-link ${pathname === page.href ? "" : "text-zinc-400"}`}>
-                                {page.label}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
             </div>
         </nav>
     );
