faef20ff67aa98f8e46a6dc2bc5afc00d7cb7dde
JstVex <heinhtetlulu14@gmail.com>
2024-01-20 14:31:44 -0800

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,38 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Hein Htet Lu Lu',
-  description: 'My personal website/portfolio',
+  description: 'Web Developer',
+  openGraph: {
+    title: 'Hein Htet Lu Lu',
+    description: 'Web Developer',
+    url: `https://heinhtetlulu.vercel.app`,
+    siteName: 'Hein Htet Lu Lu',
+    locale: 'en-US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: [
+      {
+        url: '/light-icon.png',
+        media: '(prefers-color-scheme: light)',
+      },
+      {
+        url: '/dark-icon.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
