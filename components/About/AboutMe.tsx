bda2697e64ecadb0723bc69b4f928ebf0895fdc7
JstVex <heinhtetlulu14@gmail.com>
2024-01-19 23:10:32 -0800

    Minor text update

--- components/About/AboutMe.tsx
+++ components/About/AboutMe.tsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
                     I design and develop websites with the goal of solving problems, making impact and crafting compelling digital experiences. Programming is an integral part of my everyday life, and I believe that it has the power to shape and improve the world around us.
                 </p>
                 <p>
-                    I aim to create softwares that are not only functional and performant, but also beautiful and intuitive. In addition to my work, I also enjoy a calm place to relax and enjoy the nature which help to inspire me in my creative process.
+                    I aim to create softwares that are not only functional and performant, but also beautiful and intuitive. In addition to my work, I also enjoy calming environments which help to inspire me in my creative process.
                 </p>
             </div>
         </section>
