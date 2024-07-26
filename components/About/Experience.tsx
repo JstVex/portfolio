2128f16bdcb2e725bf23e9f40b2d89f6c43d7af4
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-07-26 14:09:32 -0700

    Add Headstarter experience 🚀

--- components/About/Experience.tsx
+++ components/About/Experience.tsx
@@ -9,7 +9,7 @@ const Experience = () => {
                         Mobile App Development Intern @ Pasadena City College
                     </h3>
                     <div className="text-sm text-zinc-400">
-                        April 2024 - Ongoing
+                        April 2024 - July 2024
                     </div>
                 </div>
                 <p>
@@ -19,17 +19,30 @@ const Experience = () => {
             <li className="flex flex-col space-y-1 pb-2 md:space-y-3 md:border-b md:border-zinc-900 md:pb-10">
                 <div>
                     <h3 className="text-base font-semibold">
+                        Software Engineering Fellow @ Headstarter
+                    </h3>
+                    <div className="text-sm text-zinc-400">
+                        July 2024 - September 2024
+                    </div>
+                </div>
+                <p>
+                    Building an AI-related software with the goal of 1000 users while learning from industry professionals and participating in weekly hackathons
+                </p>
+            </li>
+            <li className="flex flex-col space-y-1 pb-2 md:space-y-3 md:border-b md:border-zinc-900 md:pb-10">
+                <div>
+                    <h3 className="text-base font-semibold">
                         Volunteer / Web Developer @ Conceptians
                     </h3>
                     <div className="text-sm text-zinc-400">
-                        June 2023 - Ongoing
+                        June 2023 - January 2024
                     </div>
                 </div>
                 <p>
                     Designing and developing websites for non-profit organizations
                 </p>
             </li>
-            <li className="flex flex-col space-y-1 pb-6 md:space-y-3 md:pb-10">
+            <li className="flex flex-col space-y-1 pb-2 md:space-y-3 md:border-b md:border-zinc-900  md:pb-10">
                 <div>
                     <h3 className="text-base font-semibold">
                         Intern @ Amara Digital Marketing Agency
@@ -42,7 +55,7 @@ const Experience = () => {
                     Built digital solutions for local businesses
                 </p>
             </li>
-            <li className="flex flex-col space-y-1 pb-2 md:space-y-3 md:border-b md:border-zinc-900  md:pb-10">
+            <li className="flex flex-col space-y-1 pb-6 md:space-y-3 md:pb-10">
                 <div>
                     <h3 className="text-base font-semibold">
                         Freelance
